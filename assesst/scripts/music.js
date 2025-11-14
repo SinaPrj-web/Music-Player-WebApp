df1172ac488503ff03abb30002dedeea2f6658e9
Sina Prj <your-email@example.com>
2025-11-14 19:34:08 +0330

    add song array

---
+++ assesst/scripts/music.js
@@ -0,0 +1,7 @@
+let songs = [
+    {title: 'lovely', artist: 'Billie Eilish Ft Khalid', src: '../songs/Billie-Eilish-Ft-Khalid-lovely.mp3'},
+    {title: 'i love you', artist: 'Billie Eilish', src: '../songs/Billie-Eilish-I-Love-You.mp3'},
+    {title: 'Fear of the water', artist: 'SYML', src: '../songs/Fear-of-the-Water-SYML.mp3'},
+    {title: 'Sbowfall', artist: 'Øneheart', src: '../songs/one-heart-snowfall.mp3'},
+    {title: 'Fourth of july', artist: 'Sufjan Stevens', src: '../songs/Sufjan-Stevens-fourth-of-july.mp3'},
+]
