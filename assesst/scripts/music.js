f26c69ffaf79ef1cb7cf24c36be7b7e29990c6da
Sina Prj <your-email@example.com>
2025-11-17 20:21:41 +0330

    add style to container and BG

--- assesst/scripts/music.js
+++ assesst/scripts/music.js
@@ -4,4 +4,4 @@ let songs = [
     {title: 'Fear of the water', artist: 'SYML', src: '../songs/Fear-of-the-Water-SYML.mp3'},
     {title: 'Sbowfall', artist: 'Øneheart', src: '../songs/one-heart-snowfall.mp3'},
     {title: 'Fourth of july', artist: 'Sufjan Stevens', src: '../songs/Sufjan-Stevens-fourth-of-july.mp3'},
-]
+];
