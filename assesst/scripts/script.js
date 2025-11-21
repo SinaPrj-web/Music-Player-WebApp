3a8016d61a0b1a6fffb30c4a941e451c39a32ae0
Sina Prj <your-email@example.com>
2025-11-21 22:45:13 +0330

    change music name

--- assesst/scripts/script.js
+++ assesst/scripts/script.js
@@ -1,1 +1,15 @@
+let $ = document;
+import songs from "./music.js";
 
+const audio = $.querySelector('#audio')
+const playBtn = $.querySelector('#play-btn')
+const pauseBtn = $.querySelector('#pause-btn')
+
+let currentIndex = 0
+audio.src = songs[currentIndex].src
+audio.load();
+
+
+playBtn.addEventListener('click' , function () {
+    audio.play();
+})
