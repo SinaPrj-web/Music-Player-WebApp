1d14f90b750a194c39397fe791d1a65f7b5cc157
Sina Prj <your-email@example.com>
2025-11-23 17:03:33 +0330

    change some music path and basicJS

--- assesst/scripts/music.js
+++ assesst/scripts/music.js
@@ -1,9 +1,9 @@
 const songs = [
-    {title: 'lovely', artist: 'Billie Eilish Ft Khalid', src: 'assest/songs/Billie-Eilish-Ft-Khalid-lovely.mp3'},
-    {title: 'i love you', artist: 'Billie Eilish', src: '../songs/Billie-Eilish-I-Love-You.mp3'},
-    {title: 'Fear of the water', artist: 'SYML', src: '../songs/Fear-of-the-Water-SYML.mp3'},
-    {title: 'Sbowfall', artist: 'Øneheart', src: '../songs/one-heart-snowfall.mp3'},
-    {title: 'Fourth of july', artist: 'Sufjan Stevens', src: '../songs/Sufjan-Stevens-fourth-of-july.mp3'},
+    {title: 'lovely', artist: 'Billie Eilish Ft Khalid', src: 'assesst/songs/Billie-Eilish-Ft-Khalid-lovely.mp3' , poster: 'assesst/images/lovely-cover.jpg'},
+    {title: 'i love you', artist: 'Billie Eilish', src: 'assesst/songs/Billie-Eilish-I-Love-You.mp3' ,  poster: 'assesst/images/billie-eilish-cover.webp'},
+    {title: 'Fear of the water', artist: 'SYML', src: 'assesst/songs/Fear-of-the-Water-SYML.mp3', poster: 'assesst/images/fear-the-water-cover.jpg'},
+    {title: 'Sbowfall', artist: 'Øneheart', src: 'assesst/songs/one-heart-snowfall.mp3', poster: 'assesst/images/one-heart-cover.jpg'},
+    {title: 'Fourth of july', artist: 'Sufjan Stevens', src: 'assesst/songs/Sufjan-Stevens-fourth-of-july.mp3', poster: 'assesst/images/fourth-of-july-cover.webp'},
 ]; 
 
 export default songs
--- assesst/scripts/script.js
+++ assesst/scripts/script.js
@@ -3,13 +3,102 @@ import songs from "./music.js";
 
 const audio = $.querySelector('#audio')
 const playBtn = $.querySelector('#play-btn')
-const pauseBtn = $.querySelector('#pause-btn')
+const musicName = $.querySelector('.music-name')
+const artistName = $.querySelector('.artist-name')
+const nextBtn = $.querySelector('#next-song')
+const prevBtn = $.querySelector('#prev-song')
+const songPoster = $.querySelector('#song-poster')
+const playToggle = $.querySelector('.play-btn')
+
 
 let currentIndex = 0
-audio.src = songs[currentIndex].src
-audio.load();
+let isPlaying = false 
 
+
+// load song
+function loadSongs (index) {
+    audio.src = songs[index].src
+    musicName.textContent = songs[index].title
+    artistName.textContent = songs[index].artist
+    songPoster.src = songs[index].poster
+    audio.load
+    checkTitleOverflow()
+}
+
+// nextBtn function
+function nextSongBtn () {
+    currentIndex ++
+    if(currentIndex >= songs.length) currentIndex = 0;
+    loadSongs(currentIndex)
+    audio.play();
+    checkTitleOverflow()
+    updateIcon()
+
+}
+// prev function
+function prevSong () {
+    currentIndex --
+    if (currentIndex < 0)currentIndex = songs.length - 1
+    loadSongs(currentIndex)
+    audio.play();
+    checkTitleOverflow()
+    updateIcon()
+}
+
+// titleOverflow function
+
+function checkTitleOverflow () {
+    const nameWrapper = $.querySelector('.music-name-container')
+    const title = $.querySelector('.music-name')
+
+    if(title.scrollWidth > nameWrapper.clientWidth) {
+        title.classList.add('scroll')
+    } else {
+        title.classList.remove('scroll')
+    }
+}
+
+// play toggle btn function 
+playToggle.addEventListener('click' , function () {
+    if(!isPlaying){
+        audio.play();
+        isPlaying = true;
+        playToggle.innerHTML = '<i id="play-btn" class="fa-solid fa-pause"></i>'
+    } else {
+        audio.pause();
+        isPlaying = false;
+        playToggle.innerHTML = '<i id="play-btn" class="fa-solid fa-play"></i>'
+    }
+})
+
+
+// update play button function
+function updateIcon () {
+    if(isPlaying) {
+        playBtn.className = 'fa fa-pause'
+
+    } else {
+        playBtn.className = 'fa fa-play'
+    }
+
+}
+
+audio.addEventListener('play' , ()=> {
+    isPlaying = true;
+    updateIcon()
+})
+
+audio.addEventListener('pause' , ()=> {
+    isPlaying = false;
+    updateIcon()
+})
 
 playBtn.addEventListener('click' , function () {
-    audio.play();
+    audio.play().catch(err => console.error('play error' , err))
+
 })
+
+
+nextBtn.addEventListener('click' , nextSongBtn)
+prevBtn.addEventListener('click' ,prevSong)
+loadSongs(currentIndex)
